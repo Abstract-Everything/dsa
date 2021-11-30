9f4b10ca48330b69c608bb6ed2abdf35eff6535a
Jonathan Camilleri <jon.d.cam@gmail.com>
2021-11-30 23:26:11 +0100

    [visual] Create abstract widget
    This addition will make managing properties of widgest more manageable.

---
+++ visual/widgets/widget.hpp
@@ -0,0 +1,38 @@
+#ifndef VISUAL_WIDGET_HPP
+#define VISUAL_WIDGET_HPP
+
+#include <SFML/Graphics.hpp>
+
+#include <cstddef>
+
+namespace visual
+{
+
+class Widget : public sf::Drawable
+{
+ public:
+	explicit Widget(
+	    float            padding    = 0.0F,
+	    const sf::Color &background = sf::Color::Transparent);
+
+	[[nodiscard]] sf::Vector2f size() const;
+	void                       adjust();
+
+	void background(const sf::Color &background);
+
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+ protected:
+	[[nodiscard]] virtual sf::Vector2f content_size() const = 0;
+
+	virtual void content_draw(
+	    sf::RenderTarget &target,
+	    sf::RenderStates  states) const = 0;
+
+ private:
+	float              m_padding;
+	sf::RectangleShape m_rectangle;
+};
+
+} // namespace visual
+#endif
