78db3abc4bf9d25bc754b6d62126393ecbf56f01
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-01-11 19:44:52 +0100

    [test/dsa] List: Test default initilaisation

--- test/dsa/list.cpp
+++ test/dsa/list.cpp
@@ -4,3 +4,10 @@
 
 static_assert(std::is_same_v<dsa::List<int>::Value, int>);
 
+TEST(list, default_initialisation)
+{
+	dsa::List<int> list;
+
+	ASSERT_TRUE(list.empty());
+	ASSERT_EQ(list.size(), 0ULL);
+}
