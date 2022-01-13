9385a8b0866af93a1ceffe862f5aa3ee6b76d75c
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-01-14 00:15:35 +0100

    [test/dsa] Binary_Tree: Test default initialisation

--- test/dsa/binary_tree.cpp
+++ test/dsa/binary_tree.cpp
@@ -3,3 +3,11 @@
 #include <gtest/gtest.h>
 
 static_assert(std::is_same_v<dsa::Binary_Tree<int>::Value, int>);
+
+TEST(binary_tree, default_initialisation)
+{
+	dsa::Binary_Tree<int> binary_tree;
+
+	ASSERT_TRUE(binary_tree.empty());
+	ASSERT_EQ(binary_tree.size(), 0ULL);
+}
