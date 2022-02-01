947ba67096a220a1bfac5905ee038793380477ea
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-02-01 23:29:49 +0100

    [dsa] Binary_Tree: Implement iterator begin/ end

--- dsa/include/dsa/binary_tree.hpp
+++ dsa/include/dsa/binary_tree.hpp
@@ -25,11 +25,71 @@ class Binary_Tree_Node
 	using Pointer      = typename Alloc_Traits::Pointer;
 	using Const_Pointer = typename Alloc_Traits::Const_Pointer;
 
-	using Satellite_Alloc_Traits =
-	    Allocator_Traits<Allocator_Base<Satellite_t>>;
-	using Satellite = typename Satellite_Alloc_Traits::Value;
+	using Satellite_Alloc_Traits    = Allocator_Traits<Allocator_Base<Satellite_t>>;
+	using Satellite                 = typename Satellite_Alloc_Traits::Value;
+	using Satellite_Pointer         = typename Satellite_Alloc_Traits::Reference;
+	using Satellite_Const_Pointer   = typename Satellite_Alloc_Traits::Const_Reference;
+	using Satellite_Reference       = typename Satellite_Alloc_Traits::Reference;
+	using Satellite_Const_Reference = typename Satellite_Alloc_Traits::Const_Reference;
+
+	template<bool Is_Const>
+	class Iterator_Detail
+	{
+	 private:
+		using Node_Pointer = std::conditional_t<
+		    Is_Const,
+		    typename Binary_Tree_Node::Const_Pointer,
+		    typename Binary_Tree_Node::Pointer>;
+
+		using Pointer = std::conditional_t<
+		    Is_Const,
+		    typename Binary_Tree_Node::Satellite_Const_Pointer,
+		    typename Binary_Tree_Node::Satellite_Pointer>;
+
+		using Reference = std::conditional_t<
+		    Is_Const,
+		    typename Binary_Tree_Node::Satellite_Const_Reference,
+		    typename Binary_Tree_Node::Satellite_Reference>;
+
+	 public:
+		explicit Iterator_Detail(Node_Pointer node) : m_node(node)
+		{
+		}
+
+		Iterator_Detail operator++()
+		{
+			Iterator_Detail iterator(m_node);
+			return iterator;
+		}
+
+		bool operator==(Iterator_Detail const &iterator) const
+		{
+			return m_node == iterator.m_node;
+		}
+
+		bool operator!=(Iterator_Detail const &iterator) const
+		{
+			return !this->operator==(iterator);
+		}
+
+		Reference operator*() const
+		{
+			return m_node->m_satellite;
+		}
+
+		Pointer operator->() const
+		{
+			return m_node->m_satellite;
+		}
+
+	 private:
+		Node_Pointer m_node;
+	};
 
  public:
+	using Iterator       = Iterator_Detail<false>;
+	using Const_Iterator = Iterator_Detail<true>;
+
 	Satellite m_satellite;
 	Pointer   m_parent;
 	Pointer   m_left;
@@ -70,9 +130,11 @@ class Binary_Tree
 	using Alloc_Traits = Allocator_Traits<Allocator_Base<Value_t>>;
 
  public:
-	using Allocator = typename Alloc_Traits::Allocator;
-	using Value     = typename Alloc_Traits::Value;
-	using Pointer   = typename Alloc_Traits::Pointer;
+	using Allocator      = typename Alloc_Traits::Allocator;
+	using Value          = typename Alloc_Traits::Value;
+	using Pointer        = typename Alloc_Traits::Pointer;
+	using Iterator       = typename Node::Iterator;
+	using Const_Iterator = typename Node::Const_Iterator;
 
 	/**
 	 * @brief Constructs an empty binary tree
@@ -133,6 +195,26 @@ class Binary_Tree
 	[[nodiscard]] bool empty() const
 	{
 		return m_head == nullptr;
+	}
+
+	[[nodiscard]] Iterator begin()
+	{
+		return Iterator(m_head);
+	}
+
+	[[nodiscard]] Const_Iterator begin() const
+	{
+		return Const_Iterator(m_head);
+	}
+
+	[[nodiscard]] Iterator end()
+	{
+		return Iterator(nullptr);
+	}
+
+	[[nodiscard]] Const_Iterator end() const
+	{
+		return Const_Iterator(nullptr);
 	}
 
 	/**
