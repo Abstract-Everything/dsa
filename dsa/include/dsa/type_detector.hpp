16dc5da4b8cdd63422960107217c27d9686a490c
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-02-05 12:59:04 +0100

    [dsa] type_detector: Support default type
    This extension allows us to provide a fallback type if the expression
    fails. This is useful in cases where a good default is known, such as a
    reference type in an allocator having 'Value&' as a default.

--- dsa/include/dsa/type_detector.hpp
+++ dsa/include/dsa/type_detector.hpp
@@ -13,28 +13,36 @@ struct No_Type
 {
 };
 
-template<typename Always_Void, template<typename...> typename Operator, typename... Arguments>
+template<typename Default_Value, typename Always_Void, template<typename...> typename Operator, typename... Arguments>
 struct Detector
 {
-	using Type    = No_Type;
+	using Type    = Default_Value;
 	using Value_t = std::false_type;
 };
 
-template<template<typename...> typename Operator, typename... Arguments>
-struct Detector<std::void_t<Operator<Arguments...>>, Operator, Arguments...>
+template<typename Default_Value, template<typename...> typename Operator, typename... Arguments>
+struct Detector<Default_Value, std::void_t<Operator<Arguments...>>, Operator, Arguments...>
 {
-	using Type    = decltype(Operator<Arguments...>());
+	using Type    = Operator<Arguments...>;
 	using Value_t = std::true_type;
 };
 
 } // namespace detail
 
 template<template<typename...> typename Operator, typename... Arguments>
-using Detect_T = typename detail::Detector<void, Operator, Arguments...>::Type;
+using Detect_T = typename detail::Detector<detail::No_Type, void, Operator, Arguments...>::Type;
 
 template<template<typename...> typename Operator, typename... Arguments>
-constexpr bool Detect_V =
-    detail::Detector<void, Operator, Arguments...>::Value_t::value;
+constexpr bool Detect_V = detail::Detector<detail::No_Type, void, Operator, Arguments...>::Value_t::value;
+
+template<typename Default_Value, template<typename...> typename Operator, typename... Arguments>
+using Detect_Default = detail::Detector<Default_Value, void, Operator, Arguments...>;
+
+template<typename Default_Value, template<typename...> typename Operator, typename... Arguments>
+using Detect_Default_T = typename Detect_Default<Default_Value, Operator, Arguments...>::Type;
+
+template<typename Default_Value, template<typename...> typename Operator, typename... Arguments>
+constexpr bool Detect_Default_V = Detect_Default<Default_Value, Operator, Arguments...>::Value_t::value;
 
 } // namespace dsa
 
