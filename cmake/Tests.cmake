22faee05f527f9b39d34b7b48f30be1f18605df0
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-02-05 11:43:35 +0100

    [cmake] Create a project for constexpr tests

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -1,11 +1,16 @@
 # Setup options for the supported test frameworks
 set(CPP_PROJECT_TEMPLATE_USING_GTEST FALSE)
+set(CPP_PROJECT_TEMPLATE_USING_CATCH FALSE)
 
 macro(ENABLE_TESTS)
 	option(DSA_RUNTIME_TESTS "Compile dsa runtime tests" FALSE)
-	set(CPP_PROJECT_TEMPLATE_USING_GTEST ${DSA_RUNTIME_TESTS})
+	option(DSA_STATIC_TESTS "Compile dsa static tests" FALSE)
+	option(DSA_STATIC_DEBUG_TESTS "Compile dsa static tests into an executable for debugging" FALSE)
 
-	if(${CPP_PROJECT_TEMPLATE_USING_GTEST})
+	set(CPP_PROJECT_TEMPLATE_USING_GTEST ${DSA_RUNTIME_TESTS})
+	set(CPP_PROJECT_TEMPLATE_USING_CATCH ${DSA_STATIC_TESTS} OR ${DSA_STATIC_DEBUG_TESTS})
+
+	if(${CPP_PROJECT_TEMPLATE_USING_GTEST} OR ${CPP_PROJECT_TEMPLATE_USING_CATCH})
 		enable_testing()
 	endif()
 endmacro()
---
+++ test/dsa/dsa_static/allocator_traits.cpp
@@ -0,0 +1,8 @@
+#include <dsa/allocator_traits.hpp>
+#include <dsa/default_allocator.hpp>
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("Created test file", "[allocator_traits]")
+{
+}
