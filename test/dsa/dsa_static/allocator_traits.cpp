4e831854e07e794169ef8405205f05935a15b1a6
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-02-05 15:20:21 +0100

    [test/dsa] Allocator_Traits: Test constexpr allocate
    Separate sections are used for constexpr and runtime contexts in order
    to force the compiler to try to create the function in both and see that
    they compile. If it compiles it constexpr it should compile in runtime,
    but constexpr is more limited so some features cannot be used.

--- test/dsa/dsa_static/allocator_traits.cpp
+++ test/dsa/dsa_static/allocator_traits.cpp
@@ -2,6 +2,8 @@
 #include <dsa/default_allocator.hpp>
 
 #include "empty_value.hpp"
+#include "id.hpp"
+#include "dummy_pointer.hpp"
 
 #include <catch2/catch.hpp>
 
@@ -16,4 +18,58 @@ TEST_CASE("Created test file", "[allocator_traits]")
 	STATIC_REQUIRE(std::is_same_v<Traits::Const_Reference, Empty_Value const &>);
 	STATIC_REQUIRE(std::is_same_v<Traits::Pointer, Empty_Value *>);
 	STATIC_REQUIRE(std::is_same_v<Traits::Const_Pointer, Empty_Value const *>);
+}
+
+namespace
+{
+
+std::size_t max_limit_size_t()
+{
+	return std::numeric_limits<std::size_t>::max();
+}
+
+struct Dummy_Allocator
+{
+	using Value   = int;
+	using Pointer = Dummy_Pointer;
+
+	constexpr Pointer allocate(std::size_t count)
+	{
+		allocated_count = count;
+		return Dummy_Pointer(Id(count));
+	}
+
+	std::size_t allocated_count = 0ULL;
+};
+
+using Traits = dsa::Allocator_Traits<Dummy_Allocator>;
+
+constexpr std::pair<Dummy_Allocator, Dummy_Pointer> allocate(std::size_t count)
+{
+	Dummy_Allocator allocator;
+	return {allocator, Traits::allocate(allocator, count)};
+}
+
+} // namespace
+
+TEST_CASE(
+    "Allocator_Traits calls custom allocator operation overloads if present",
+    "[allocator_traits]")
+{
+	SECTION("Custom allocate is called in a static context")
+	{
+		constexpr std::size_t count = 5;
+		constexpr auto        pair  = allocate(count);
+
+		STATIC_REQUIRE(pair.first.allocated_count == count);
+		STATIC_REQUIRE(pair.second.id().value() == count);
+	}
+
+	SECTION("Custom allocate is called with the forwarded count")
+	{
+		const std::size_t count   = GENERATE(0ULL, max_limit_size_t());
+		auto [allocator, pointer] = allocate(count);
+		REQUIRE(allocator.allocated_count == count);
+		REQUIRE(pointer.id().value() == count);
+	}
 }
---
+++ test/dsa/dsa_static/dummy_pointer.hpp
@@ -0,0 +1,43 @@
+#ifndef TEST_DSA_STATIC_DUMMY_POINTER_HPP
+#define TEST_DSA_STATIC_DUMMY_POINTER_HPP
+
+#include "id.hpp"
+
+class Dummy_Pointer
+{
+ public:
+	explicit constexpr Dummy_Pointer(Id id) : m_id(id)
+	{
+	}
+
+	constexpr ~Dummy_Pointer() = default;
+
+	Dummy_Pointer(Dummy_Pointer const &) = delete;
+	Dummy_Pointer(Dummy_Pointer &&)      = default;
+	Dummy_Pointer &operator=(Dummy_Pointer const &) = delete;
+	Dummy_Pointer &operator=(Dummy_Pointer &&) = default;
+
+	[[nodiscard]] constexpr friend bool operator==(
+	    Dummy_Pointer const &lhs,
+	    Dummy_Pointer const &rhs)
+	{
+		return lhs.m_id == rhs.m_id;
+	}
+
+	[[nodiscard]] constexpr friend bool operator!=(
+	    Dummy_Pointer const &lhs,
+	    Dummy_Pointer const &rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	[[nodiscard]] constexpr Id id() const
+	{
+		return m_id;
+	}
+
+ private:
+	Id m_id;
+};
+
+#endif
