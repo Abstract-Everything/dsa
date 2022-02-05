18c1122557c1d2dfd3dfefac3f7d88087ec83446
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-02-05 13:02:17 +0100

    [dsa] Allocator_Traits: Move helpers out of class
    This increases the readability of the class by removing all the
    boilerplate template code.

--- dsa/include/dsa/allocator_traits.hpp
+++ dsa/include/dsa/allocator_traits.hpp
@@ -10,24 +10,49 @@
 namespace dsa
 {
 
-template<typename Allocator_t>
-class Allocator_Traits
+namespace detail
 {
- public:
-	using Allocator       = Allocator_t;
-	using Value           = typename Allocator::Value;
-	using Reference       = typename Allocator::Reference;
-	using Const_Reference = typename Allocator::Const_Reference;
-	using Pointer         = typename Allocator::Pointer;
-	using Const_Pointer   = typename Allocator::Const_Pointer;
 
- private:
-	using Std_Allocator        = std::allocator<Value>;
-	using Std_Allocator_Traits = std::allocator_traits<Std_Allocator>;
+class Allocator_Traits_Base
+{
+ protected:
+	template<typename Allocator>
+	using Has_Reference = typename Allocator::Reference;
+
+	template<typename Default_Type, typename Allocator>
+	using Detect_Reference_T =
+	    Detect_Default_T<Default_Type, Has_Reference, Allocator>;
+
+	template<typename Allocator>
+	using Has_Const_Reference = typename Allocator::Const_Reference;
+
+	template<typename Default_Type, typename Allocator>
+	using Detect_Const_Reference_T =
+	    Detect_Default_T<Default_Type, Has_Const_Reference, Allocator>;
+
+	template<typename Allocator>
+	using Has_Pointer = typename Allocator::Pointer;
+
+	template<typename Default_Type, typename Allocator>
+	using Detect_Pointer_T =
+	    Detect_Default_T<Default_Type, Has_Pointer, Allocator>;
+
+	template<typename Allocator>
+	using Has_Const_Pointer = typename Allocator::Const_Pointer;
+
+	template<typename Default_Type, typename Allocator>
+	using Detect_Const_Pointer_T =
+	    Detect_Default_T<Default_Type, Has_Const_Pointer, Allocator>;
 
 	template<typename Allocator>
 	using Has_Allocate_Operator =
 	    decltype(std::declval<Allocator>().allocate(std::size_t()));
+
+	template<typename Allocator>
+	static constexpr bool has_allocate()
+	{
+		return Detect_V<Has_Allocate_Operator, Allocator>;
+	}
 
 	template<typename Allocator, typename Pointer, typename... Arguments>
 	using Has_Construct_Operator =
@@ -35,39 +60,54 @@ class Allocator_Traits
 		Pointer(),
 		std::declval<Arguments>()...));
 
-	template<typename Allocator>
-	using Has_Deallocate_Operator =
-	    decltype(std::declval<Allocator>().deallocate(Pointer(), std::size_t()));
-
-	template<typename Allocator, typename Pointer>
-	using Has_Destroy_Operator =
-	    decltype(std::declval<Allocator>().construct(Pointer()));
-
-	static constexpr bool has_allocate()
-	{
-		return Detect_V<Has_Allocate_Operator, Allocator>;
-	}
-
-	template<typename... Arguments>
+	template<typename Allocator, typename Pointer, typename... Arguments>
 	static constexpr bool has_construct()
 	{
 		return Detect_V<Has_Construct_Operator, Allocator, Pointer, Arguments...>;
 	}
 
+	template<typename Allocator, typename Pointer>
+	using Has_Deallocate_Operator =
+	    decltype(std::declval<Allocator>().deallocate(Pointer(), std::size_t()));
+
+	template<typename Allocator, typename Pointer>
 	static constexpr bool has_deallocate()
 	{
-		return Detect_V<Has_Deallocate_Operator, Allocator>;
+		return Detect_V<Has_Deallocate_Operator, Allocator, Pointer>;
 	}
 
+	template<typename Allocator, typename Pointer>
+	using Has_Destroy_Operator =
+	    decltype(std::declval<Allocator>().construct(Pointer()));
+
+	template<typename Allocator, typename Pointer>
 	static constexpr bool has_destroy()
 	{
 		return Detect_V<Has_Destroy_Operator, Allocator, Pointer>;
 	}
+};
+
+} // namespace detail
+
+template<typename Allocator_t>
+class Allocator_Traits : detail::Allocator_Traits_Base
+{
+ public:
+	using Allocator       = Allocator_t;
+	using Value           = typename Allocator::Value;
+	using Reference       = Detect_Reference_T<Value &, Allocator>;
+	using Const_Reference = Detect_Const_Reference_T<Value const &, Allocator>;
+	using Pointer         = Detect_Pointer_T<Value *, Allocator>;
+	using Const_Pointer   = Detect_Const_Pointer_T<Value const *, Allocator>;
+
+ private:
+	using Std_Allocator        = std::allocator<Value>;
+	using Std_Allocator_Traits = std::allocator_traits<Std_Allocator>;
 
  public:
 	static Pointer allocate(Allocator allocator, std::size_t count)
 	{
-		if constexpr (has_allocate())
+		if constexpr (has_allocate<Allocator>())
 		{
 			return allocator.allocate(count);
 		}
@@ -85,7 +125,7 @@ class Allocator_Traits
 	    Pointer                    pointer,
 	    Arguments &&...arguments)
 	{
-		if constexpr (has_construct<Arguments...>())
+		if constexpr (has_construct<Allocator, Pointer, Arguments...>())
 		{
 			allocator.construct(
 			    pointer,
@@ -93,7 +133,6 @@ class Allocator_Traits
 		}
 		else
 		{
-			// ToDo: Use construct_at
 			Std_Allocator std_allocator;
 			Std_Allocator_Traits::construct(
 			    std_allocator,
@@ -104,7 +143,7 @@ class Allocator_Traits
 
 	static void deallocate(Allocator allocator, Pointer pointer, std::size_t count)
 	{
-		if constexpr (has_deallocate())
+		if constexpr (has_deallocate<Allocator, Pointer>())
 		{
 			allocator.deallocate(pointer, count);
 		}
@@ -123,13 +162,12 @@ class Allocator_Traits
 	    [[maybe_unused]] Allocator allocator,
 	    Pointer                    pointer)
 	{
-		if constexpr (has_destroy())
+		if constexpr (has_destroy<Allocator, Pointer>())
 		{
 			allocator.destroy(pointer);
 		}
 		else
 		{
-			// ToDo: Use destroy_at
 			Std_Allocator std_allocator;
 			Std_Allocator_Traits::destroy(std_allocator, pointer);
 		}
