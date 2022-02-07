ba4242660987f90d660343b9043a91a57793d17a
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-02-07 22:44:53 +0100

    [test/dsa] Allocator_Traits: Expose custom types

--- test/dsa/dsa_static/allocator_traits.cpp
+++ test/dsa/dsa_static/allocator_traits.cpp
@@ -1,8 +1,19 @@
 #include <dsa/allocator_traits.hpp>
 #include <dsa/default_allocator.hpp>
 
+#include "empty_value.hpp"
+
 #include <catch2/catch.hpp>
 
 TEST_CASE("Created test file", "[allocator_traits]")
 {
+	using Allocator = dsa::Default_Allocator<Empty_Value>;
+	using Traits    = dsa::Allocator_Traits<Allocator>;
+
+	STATIC_REQUIRE(std::is_same_v<Traits::Allocator, Allocator>);
+	STATIC_REQUIRE(std::is_same_v<Traits::Value, Empty_Value>);
+	STATIC_REQUIRE(std::is_same_v<Traits::Reference, Empty_Value &>);
+	STATIC_REQUIRE(std::is_same_v<Traits::Const_Reference, Empty_Value const &>);
+	STATIC_REQUIRE(std::is_same_v<Traits::Pointer, Empty_Value *>);
+	STATIC_REQUIRE(std::is_same_v<Traits::Const_Pointer, Empty_Value const *>);
 }
