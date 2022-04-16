996aab33c8c83d37812375e3f5957c580c09cb10
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-04-16 18:00:39 +0200

    [dsa] implement is_heap

---
+++ dsa/include/dsa/algorithms.hpp
@@ -0,0 +1,59 @@
+#ifndef DSA_ALGORITHMS_HPP
+#define DSA_ALGORITHMS_HPP
+
+namespace dsa
+{
+
+/**
+ * @brief Checks if the given range forms a heap
+ *
+ * An array is a heap if every element and its corresponding children return
+ * true when compared using comparator(parent, child) or parent == child. The
+ * child of a parent is equal to 2 * parent_index + 1.
+ *
+ * Example, the following array relationships are as follows:
+ * Index: 0 1 2 3 4 5 6
+ * Value: a b c d e f g
+ *
+ * Relationships:
+ *         a
+ *     b       c
+ *   d   e   f   g
+ *
+ * So for example the input:
+ *   begin,          end, operator<
+ *     0, 1, 2, 1, 1, 0
+ *
+ * is not a heap because the third element (2) < its left child (0) is false
+ *
+ * Equal elements are valid because without this property a weird behaviour is
+ * introduced, consider the following: array: 0, 1, 1
+ *
+ * This clearly forms a min heap because 0 < 1. If we pop the top element we end
+ * up with 1, 1 which does not form a heap if equality is not allowed. Thus this
+ * results in popping the top element invalidating the heap
+ */
+template<typename Iterator, typename Comparator>
+bool is_heap(Iterator begin, Iterator end, const Comparator &comparator)
+{
+	bool increment_parent = false;
+	for (Iterator parent = begin, child = begin + 1; child < end; ++child)
+	{
+		if (*parent != *child && !comparator(*parent, *child))
+		{
+			return false;
+		}
+
+		if (increment_parent)
+		{
+			++parent;
+		}
+
+		increment_parent = !increment_parent;
+	}
+	return true;
+}
+
+} // namespace dsa
+
+#endif
