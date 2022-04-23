439f1990968d9c7893003dc60a3ce3651738672e
Jonathan Camilleri <jon.d.cam@gmail.com>
2022-04-23 19:44:41 +0200

    [dsa] Heap: add push
    push allows the caller to add values to the heap while maintaining its
    properties.

--- dsa/include/dsa/heap.hpp
+++ dsa/include/dsa/heap.hpp
@@ -3,21 +3,27 @@
 
 #include <dsa/allocator_traits.hpp>
 #include <dsa/default_allocator.hpp>
+#include <dsa/vector.hpp>
 
 namespace dsa
 {
 
 /**
  * @brief Represents a set of contiguous elements of the same type organised in
- * such a way as to allow fast access to the greatest element.
+ * such a way as to allow fast access to the greatest element. The heap ensures
+ * that the property comparator(root, child) holds for each element
  *
  * @ingroup containers
  *
  * @tparam Value_t: The type of element to store
+ * @tparam Comparator: The type of a comparator for which comparator(x,y) holds
  * @tparam Allocator_Base: The type of allocator used for memory management
  *
  */
-template<typename Value_t, template<typename> typename Allocator_Base = Default_Allocator>
+template<
+    typename Value_t,
+    typename Comparator                        = decltype(std::less{}),
+    template<typename> typename Allocator_Base = Default_Allocator>
 class Heap
 {
  private:
@@ -29,6 +35,72 @@ class Heap
 	using Pointer       = typename Alloc_Traits::Pointer;
 	using Const_Pointer = typename Alloc_Traits::Const_Pointer;
 
+	using Storage = dsa::Vector<Value, Allocator_Base>;
+
+	explicit Heap(Comparator comparator = std::less_equal{})
+	    : m_comparator(std::move(comparator))
+	{
+	}
+
+	/**
+	 * @brief Returns the current number of elements in the heap
+	 */
+	[[nodiscard]] std::size_t size() const
+	{
+		return m_storage.size();
+	}
+
+	/**
+	 * @brief Returns a reference to the underlying container storing the
+	 * heap
+	 */
+	[[nodiscard]] Storage &storage()
+	{
+		return m_storage;
+	}
+
+	/**
+	 * @brief Returns a const reference to the underlying container storing
+	 * the heap
+	 */
+	[[nodiscard]] const Storage &storage() const
+	{
+		return m_storage;
+	}
+
+	/**
+	 * @brief Adds an element to the heap and does the work necessary to
+	 * maintain the heap property
+	 */
+	void push(Value value)
+	{
+		using std::swap;
+
+		std::size_t index = m_storage.size();
+		m_storage.append(std::move(value));
+
+		for (std::size_t parent = parent_index(index);
+		     index != 0
+		     && !m_comparator(m_storage[parent], m_storage[index]);
+		     parent = parent_index(index))
+		{
+			swap(m_storage[parent], m_storage[index]);
+			index = parent;
+		}
+	}
+
+ private:
+	Storage    m_storage;
+	Comparator m_comparator;
+
+	/**
+	 * @brief Returns the index of the parent given an index of one of the
+	 * children.
+	 */
+	[[nodiscard]] std::size_t parent_index(std::size_t index) const
+	{
+		return (index - 1) / 2;
+	}
 };
 
 } // namespace dsa
--- test/dsa/dsa_runtime/heap.cpp
+++ test/dsa/dsa_runtime/heap.cpp
@@ -1,9 +1,80 @@
+#include <dsa/algorithms.hpp>
+#include <dsa/default_allocator.hpp>
 #include <dsa/heap.hpp>
+
+#include <fmt/format.h>
 
 #include <memory>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-using Int_Heap = dsa::Heap<int>;
+using namespace testing;
 
-static_assert(std::is_same_v<Int_Heap::Value, int>);
+namespace
+{
+
+MATCHER(is_heap, fmt::format("to {}be a min heap", negation ? "not " : ""))
+{
+	return dsa::is_heap(arg.storage().begin(), arg.storage().end(), std::less{});
+}
+
+} // namespace
+
+static_assert(std::is_same_v<dsa::Heap<int>::Value, int>);
+
+TEST(heap, default_initialisation)
+{
+	dsa::Heap<int> heap;
+
+	ASSERT_EQ(heap.size(), 0ULL);
+	ASSERT_THAT(heap, is_heap());
+}
+
+TEST(heap, push_single_value)
+{
+	dsa::Heap<int> heap;
+	heap.push(0);
+
+	ASSERT_EQ(heap.size(), 1ULL);
+	ASSERT_THAT(heap, is_heap());
+}
+
+TEST(heap, push_value_is_in_order)
+{
+	dsa::Heap<int> heap;
+	heap.push(1);
+	heap.push(3);
+	heap.push(5);
+	heap.push(7);
+
+	ASSERT_EQ(heap.size(), 4ULL);
+	ASSERT_THAT(heap, is_heap());
+}
+
+TEST(heap, push_value_swaps_parent)
+{
+	dsa::Heap<int> heap;
+	heap.push(1);
+	heap.push(3);
+	heap.push(5);
+	heap.push(2);
+
+	ASSERT_EQ(heap.size(), 4ULL);
+	ASSERT_THAT(heap, is_heap());
+}
+
+TEST(heap, push_value_propogated_to_root)
+{
+	dsa::Heap<int> heap;
+	heap.push(1);
+	heap.push(2);
+	heap.push(3);
+	heap.push(4);
+	heap.push(5);
+	heap.push(6);
+	heap.push(0);
+
+	ASSERT_EQ(heap.size(), 7ULL);
+	ASSERT_THAT(heap, is_heap());
+}
