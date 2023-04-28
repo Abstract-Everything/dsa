360c6cff3d3eb34a1d0dc5eeceaff1eb94591fe4
Jonathan Camilleri <jon.d.cam@gmail.com>
2023-04-28 17:51:45 +0200

    [test] Separate element_monitor tests from memory_monitor_tests
    It makes it easier to test the element_monitor itself rather than
    through the memory monitor.

--- dsa/include/dsa/type_traits.hpp
+++ dsa/include/dsa/type_traits.hpp
@@ -13,6 +13,24 @@ template<typename Non_Const_T, typename Const_T>
 constexpr bool Is_Const_Version_Of =
     Has_Const<Const_T> && std::is_same_v<Non_Const_T, std::remove_const_t<Const_T>>;
 
+template<typename Type, typename... Arguments>
+struct Is_Same : std::false_type
+{
+};
+
+template<typename Type, typename T>
+struct Is_Same<Type, T>
+    : std::conditional_t<
+	  std::is_same_v<std::decay_t<Type>, std::decay_t<T>>,
+	  std::true_type,
+	  std::false_type>
+{
+};
+
+template<typename Type, typename... Arguments>
+constexpr bool Is_Same_v = Is_Same<Type, Arguments...>::value;
+
+
 } // namespace dsa
 
 #endif
---
+++ test/dsa/utilities/memory_monitor_event_handler_fixture.hpp
@@ -0,0 +1,54 @@
+#ifndef TEST_DSA_STATIC_MEMORY_MONITOR_EVENT_HANDLER_FIXTURE_HPP
+#define TEST_DSA_STATIC_MEMORY_MONITOR_EVENT_HANDLER_FIXTURE_HPP
+
+#include "memory_monitor_event_handler.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace test
+{
+
+class Memory_Monitor_Event_Handler_Fixture
+{
+ public:
+	Memory_Monitor_Event_Handler_Fixture()
+	{
+		if (Event_Handler::instance() != nullptr)
+		{
+			std::cerr << "The handler's lifetime should "
+				     "only be controlled by this object";
+			std::terminate();
+		}
+
+		Event_Handler::instance() = std::make_unique<Event_Handler>();
+	}
+
+	~Memory_Monitor_Event_Handler_Fixture()
+	{
+		if (Event_Handler::instance() == nullptr)
+		{
+			std::cerr << "The handler's lifetime should only be "
+				     "controlled by this object";
+			std::terminate();
+		}
+
+		REQUIRE_NOTHROW(Event_Handler::instance()->cleanup());
+		Event_Handler::instance() = nullptr;
+	}
+
+	Memory_Monitor_Event_Handler_Fixture(
+	    Memory_Monitor_Event_Handler_Fixture const &) = delete;
+
+	Memory_Monitor_Event_Handler_Fixture &operator=(
+	    Memory_Monitor_Event_Handler_Fixture const &) = delete;
+
+	Memory_Monitor_Event_Handler_Fixture(
+	    Memory_Monitor_Event_Handler_Fixture &&) = delete;
+
+	Memory_Monitor_Event_Handler_Fixture &operator=(
+	    Memory_Monitor_Event_Handler_Fixture &&) = delete;
+};
+
+}
+
+#endif
